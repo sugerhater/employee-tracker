3d3bfc98ef9155a1d565848f45679e60a3055c42
sugerhater <kehong0926@gmail.com>
2021-01-14 17:05:06 -0500

    basic set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({ 
+  host:'localhost',
+  port:3306,
+  user:'root',
+  password:'AdalynnTatiannaKehong',
+  database: ''
+});
+
+connection.connect((err)=>{
+  if (err) throw err;
+  
+})
